106f9082cbd6c83c3d8cb1de12a86343360b09a9
Shaun Smith <disgone@gmail.com>
2022-11-22 23:19:01 -0600

    Port weather forecast enpoints to route group

---
+++ minimal-webapi-example/WeatherEndpoints.cs
@@ -0,0 +1,35 @@
+namespace MinimumApiExample;
+
+internal static class WeatherEndpoints
+{
+    internal static RouteGroupBuilder AddWeatherEndpoints(this RouteGroupBuilder routeGroup)
+    {
+        routeGroup.WithOpenApi();
+
+        routeGroup.MapGet("/weatherforecast", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        (
+                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            Random.Shared.Next(-20, 55),
+                            Summaries[Random.Shared.Next(Summaries.Length)]
+                        ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .Produces<WeatherForecast>();
+
+        return routeGroup;
+    }
+    
+    private static readonly string[] Summaries = {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    
+    internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
